02ac5b390a308b06335c472ff75f9b000a1bd0a1
nickthorpe71 <nickthorpe71@gmail.com>
2020-08-21 09:57:40 -0500

    init commit

---
+++ Stack.js
@@ -0,0 +1,21 @@
+export default class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(item) {
+
+  }
+
+  pop() {
+
+  }
+
+}
+
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
